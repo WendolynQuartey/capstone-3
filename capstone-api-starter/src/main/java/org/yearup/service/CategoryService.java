ec9057c9d59068d0dca831a8af3bea09c067bd8d
WendolynQuartey <quarteywendolyn@gmail.com>
2026-06-22 21:23:50 -0400

    get all categories fixed

--- capstone-api-starter/src/main/java/org/yearup/service/CategoryService.java
+++ capstone-api-starter/src/main/java/org/yearup/service/CategoryService.java
@@ -1,5 +1,7 @@
 package org.yearup.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.yearup.models.Category;
 import org.yearup.repository.CategoryRepository;
@@ -19,7 +21,7 @@ public class CategoryService
     public List<Category> getAllCategories()
     {
         // get all categories
-        return null;
+        return this.categoryRepository.findAll();
     }
 
     public Category getById(int categoryId)
